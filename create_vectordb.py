8add086e11bb41ac60487417fe85265127da1a9c
gulni-hal <gulni.eruslu@gmail.com>
2026-03-08 17:33:59 +0300

    made chroma.db file deleted the hf token from the embeddings

---
+++ create_vectordb.py
@@ -0,0 +1,34 @@
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+pdf_path = "assets/deneme7SON.pdf"
+
+loader = PyPDFLoader(pdf_path)
+pages = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=150
+)
+
+docs = splitter.split_documents(pages)
+
+embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+vectordb = Chroma.from_documents(
+    docs,
+    embedding=embeddings,
+    persist_directory="./chroma_db"
+)
+
+vectordb.persist()
+
+print("Chroma DB oluşturuldu.")
--- initialize.py
+++ initialize.py
@@ -7,7 +7,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-from langchain_huggingface import HuggingFaceInferenceAPIEmbeddings
 
 from dotenv import load_dotenv
 import os
@@ -33,8 +32,7 @@ def initialize_qa_system():
     docs = splitter.split_documents(pages)
 
     # Embeddings
-    embeddings = HuggingFaceInferenceAPIEmbeddings(
-        api_key=os.getenv("HF_TOKEN"), 
+    embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
